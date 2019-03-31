d4f3f06c2ef53e2dd24510d268b29aa53183de3d
Luke Geiken <luke.geiken@gmail.com>
2019-03-31 16:56:49 -0700

    added more tests to show how it works as you exand out the length

--- scripts/justify.ts
+++ scripts/justify.ts
@@ -22,3 +22,13 @@ function runJusifyTest(line: string, length: number): void {
 runJusifyTest('The quick brown fox jumps over the lazy dog.', 52);
 runJusifyTest('The quick brown fox jumps over the lazy dog.', 53);
 runJusifyTest('The quick brown fox jumps over the lazy dog.', 54);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 55);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 56);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 57);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 58);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 59);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 60);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 61);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 62);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 63);
+runJusifyTest('The quick brown fox jumps over the lazy dog.', 64);
