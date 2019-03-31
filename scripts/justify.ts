e24a1efd789e2589315814aaa5302096dcf1658f
Luke Geiken <luke.geiken@gmail.com>
2019-03-31 15:42:37 -0700

    init commit

---
+++ scripts/justify.ts
@@ -0,0 +1,4 @@
+import { TextFormatter } from './text-formatter';
+
+var formatter = new TextFormatter();
+console.log(formatter.justify('string', 10).toString());
---
+++ scripts/text-formatter.ts
@@ -0,0 +1,14 @@
+/**
+ * Text formatter class
+ */
+export class TextFormatter {
+    /**
+     * Creates a string buffer where 1st character is at 0 index of buffer 
+     * and last character is at given length
+     * @param line The string to build into the buffer
+     * @param length The index the last character is on
+     */
+    public justify(line: string, length: number): string {
+        return '';
+    }
+}
